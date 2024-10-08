401b4686c0de863247627e287e28672450965c3d
ketrabx1x <ketrabx1x@gmail.com>
2024-10-08 23:13:16 +0200

    FirstPage screen expansion

--- main.py
+++ main.py
@@ -3,9 +3,32 @@ from kivy.lang import Builder
 from database import *
 from kivymd.uix.screenmanager import MDScreenManager
 from kivymd.uix.screen import MDScreen
+from kivymd.uix.dialog import MDDialog
+from kivymd.uix.button import MDRoundFlatButton
 
 class FirstPage(MDScreen):
-    pass
+    dialog = None
+
+    def check_and_proceed(self):
+        if not self.ids.group_input.text:
+            if not self.dialog:
+                self.dialog = MDDialog(
+                    title='Alert',
+                    text='Please enter a group!',
+                    buttons=[
+                        MDRoundFlatButton(
+                            text="OK",
+                            on_release=self.close_dialog
+                        ),
+                    ],
+                )
+            self.dialog.open()
+        else:
+            self.manager.current = 'main'
+
+    def close_dialog(self, instance):
+        self.dialog.dismiss()
+        self.dialog = None  # Reset the dialog instance
 
 class MainPage(MDScreen):
     pass
