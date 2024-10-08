c3c33087f254ed524618c3e05dc7eb2a8c1efb30
ketrabx1x <ketrabx1x@gmail.com>
2024-10-08 15:01:49 +0200

    created main.kv file

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("hello world")
+from kivymd.app import MDApp
+from kivy.lang import Builder
+
+class MyApp(MDApp):
+    def build(self):
+        return Builder.load_file("main.kv")
+
+if __name__=="__main__":
+    MyApp().run()
